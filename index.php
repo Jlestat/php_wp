fae4e46e1c9ec605af6ebeeba25364b2d357f66b
Jlestat <jlestatt@gmail.com>
2023-12-01 23:16:27 +0300

    switch

--- index.php
+++ index.php
@@ -9,5 +9,30 @@
 //else{
 //    echo "ровна нулю";
 //}
-
+?>
+<?php
+$day = readline('day = ');
+switch($day){
+    case 1:
+        echo 'Понедельник';
+        break;
+    case 2:
+        echo 'Вторник';
+        break;
+    case 3:
+        echo 'Среда';
+        break;
+    case 4:
+        echo 'Четверг';
+        break;
+    case 5:
+        echo 'Пятница';
+        break;
+    case 6:
+        echo 'Суббота';
+        break;
+    case 7:
+        echo 'Воскресение';
+        break;
+}
 ?>
